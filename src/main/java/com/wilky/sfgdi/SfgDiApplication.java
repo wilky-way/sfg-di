7fb232a0b00ba1e67cea1bf5c4a979b714812d69
wilky-way <wilkyway901@gmail.com>
2020-03-12 16:28:36 -0500

    The Spring Context Example

--- src/main/java/com/wilky/sfgdi/SfgDiApplication.java
+++ src/main/java/com/wilky/sfgdi/SfgDiApplication.java
@@ -1,13 +1,22 @@
 package com.wilky.sfgdi;
 
+import com.wilky.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello(); // "Hello World!!!"
+
+		System.out.println(greeting); // Hi Folks!
 	}
 
 }
